18d01ea47f11de8a7b6721f8b33938e73acea91e
Ignacio Bardin <ilbardin@gmail.com>
2023-07-19 10:01:13 -0300

    Add polygon models.

---
+++ src/main/java/org/example/models/Vertex.java
@@ -0,0 +1,14 @@
+package org.example.models;
+
+public class Vertex {
+
+    double x;
+    double y;
+    double z;
+
+    public Vertex(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
